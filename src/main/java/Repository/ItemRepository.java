fe180bb5ad2d00f5dcca349e46020bc59d60e379
Megha Agarwal <meghaagr>
2017-05-11 13:17:17 +0530

    initializing inventory management project with flyway integration

---
+++ src/main/java/Repository/ItemRepository.java
@@ -0,0 +1,12 @@
+package Repository;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: meghaagr
+ * Date: 5/5/17
+ * Time: 3:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ItemRepository {
+
+}
