81ea2a210093c657d37d8d5ec4fb442d779525cf
Megha Agarwal <meghaagr>
2017-05-30 22:24:36 +0530

    Added test for POST request. Made item name and item quantity as composite key

--- src/main/resources/db/migration/V1__Create_item_table.sql
+++ src/main/resources/db/migration/V1__Create_item_table.sql
@@ -2,5 +2,6 @@ create table ITEM (
     ID  SERIAL PRIMARY KEY,
     NAME varchar(100) not null ,
     QUALITY varchar(100) not null,
-    PRICE DECIMAL
+    PRICE DECIMAL,
+    UNIQUE(NAME,QUALITY)
 );
--- src/test/java/com/megs/controllers/ItemControllerTest.java
+++ src/test/java/com/megs/controllers/ItemControllerTest.java
@@ -1,5 +1,6 @@
 package com.megs.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.megs.models.Item;
 import com.megs.services.IItemService;
 import org.junit.Before;
@@ -8,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -17,11 +19,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 
 import static junit.framework.TestCase.assertTrue;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -43,14 +45,9 @@ public class ItemControllerTest {
     }
 
     @Test
-    public void test_should_return_true() {
-        ArrayList<Item> items = new ArrayList<Item>();
-        Item item = new Item("item1", "quality1", new BigDecimal(23.56));
-        items.add(item);
-        when(itemService.GetItems()).thenReturn(items);
+    public void testShouldRetrieveItems() {
         try {
             ResultActions resultActions = this.mockMvc.perform(get("/items"));
-//            resultActions.andExpect(jsonPath("$[0].id", is(1)));
             boolean containsName = resultActions.andReturn().getResponse().getContentAsString().contains("\"name\":\"Item3\"");
             boolean containsId = resultActions.andReturn().getResponse().getContentAsString().contains("\"id\":1");
             assertTrue(containsName);
@@ -60,6 +57,14 @@ public class ItemControllerTest {
         }
 
     }
+
+   @Test
+    public void testShouldSaveItemAndReturnTrue() throws Exception {
+       Item item = new Item("Item2", "quality1", new BigDecimal(555.34));
+       ObjectMapper objectMapper = new ObjectMapper();
+       String s = objectMapper.writeValueAsString(item);
+       this.mockMvc.perform(post("/items").contentType(MediaType.APPLICATION_JSON).content(s)).andExpect(status().isOk()).andReturn();
+   }
 
 
 }
